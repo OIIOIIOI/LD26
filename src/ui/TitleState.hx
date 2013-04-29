db6a77978006d4520fb2f8eef3a2182a511209be
OIOIIOI <sylvere.armange@gmail.com>
2013-04-29 16:46:46 +0200

    ch

--- src/ui/TitleState.hx
+++ src/ui/TitleState.hx
@@ -4,7 +4,9 @@ import flash.events.MouseEvent;
 import flash.text.AntiAliasType;
 import flash.text.TextField;
 import flash.text.TextFormat;
+import flash.text.TextFormatAlign;
 import SoundManager;
+import utils.FTimer;
 
 /**
  * ...
@@ -14,6 +16,12 @@ import SoundManager;
 class TitleState extends State {
 	
 	var button:Button;
+	var creditentries :Array<String>;
+	var creditCounter :Int;
+	var creditfield:TextField;
+	var creditfieldformat :TextFormat;
+	
+	
 	
 	public function new () {
 		super();
@@ -30,6 +38,20 @@ class TitleState extends State {
 		tf.width = 200;
 		tf.y = 100;
 		addChild(tf);*/
+		
+		creditCounter = -1;
+		creditentries = ["Gamedesign: The whole Gagabu team", "Main programer: 01101101", "Support programer: Grmpf", "Graphic designer: Noc"];
+		creditfield = new TextField();
+		creditfieldformat = new TextFormat("OrbMedium", 15);
+		creditfield.embedFonts = true;
+		creditfield.antiAliasType = AntiAliasType.ADVANCED;
+		creditfield.selectable = false;
+		creditfield.multiline = false;
+		creditfieldformat.align = TextFormatAlign.CENTER;
+		creditfield.defaultTextFormat = creditfieldformat;
+		creditfield.width = 600;
+		creditfield.y = 450;
+		
 	}
 	
 	override public function activate () :Void {
@@ -37,6 +59,8 @@ class TitleState extends State {
 		
 		button.addEventListener(MouseEvent.CLICK, clickHandler);
 		addChild(button);
+		addChild(creditfield);
+		FTimer.delay(refreshCredit, 75);
 	}
 	
 	override public function deactivate () :Void {
@@ -44,12 +68,24 @@ class TitleState extends State {
 		
 		button.removeEventListener(MouseEvent.CLICK, clickHandler);
 		removeChild(button);
+		removeChild(creditfield);
+		FTimer.clear;
 	}
 	
 	private function clickHandler (e:MouseEvent) :Void {
 		Game.me.selectState(Game.me.prepaState);
 	}
 	
+	function refreshCredit ():Void {
+			if (creditCounter < 3) {
+				creditCounter++;
+			}
+			else {
+				creditCounter = 0;
+			}
+			creditfield.text = creditentries[creditCounter];
+			FTimer.delay(refreshCredit, 80);
+	}
 }
 
 
